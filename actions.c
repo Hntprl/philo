b3820def81be8a60526473f1b71f718d7e1f2522
Abdallah Marouf <amarouf@e1r13p3.1337.ma>
2024-08-23 20:18:27 +0100

    comit

--- actions.c
+++ actions.c
@@ -12,43 +12,35 @@
 
 #include "philo.h"
 
+void	ft_printstate(char *str, int id, size_t start_time)
+{
+	size_t i = ft_gettime() - start_time;
+	printf("%ld %d %s", i, id, str);
+}
 
 void eat(t_philo *philo)
 {
-	int death_count;
-
 	if (philo->id % 2 == 0)
 		usleep(700 * 1000);
-	pthread_mutex_lock(&philo->table->eat_mutex);
-	death_count = philo->eat_start - ft_gettime();
-	if (death_count > philo->table->time_to_die)
-		philo->table->death = 1;
-    size_t i = ft_gettime() - philo->table->start_time;
-	philo->eat_start = i;
-	philo->l_fork = &philo->table->forks[philo->id + 1];
-	pthread_mutex_lock(philo->l_fork);
-	printf("%ld %d has taken a fork\n",i , philo->id);
-	philo->r_fork = &philo->table->forks[philo->id % philo->table->number_of_philosophers];
 	pthread_mutex_lock(philo->r_fork);
-	printf("%ld %d has taken a fork\n",i , philo->id);
-	printf("%ld %d is eating\n",i , philo->id);
+	ft_printstate("has taken a fork\n", philo->id, philo->table->start_time);
+	pthread_mutex_lock(philo->l_fork);
+	ft_printstate("has taken a fork\n", philo->id, philo->table->start_time);
+	ft_printstate("is eating\n", philo->id, philo->table->start_time);
 	usleep(philo->table->time_to_eat * 1000);
 	philo->eat_num += 1;
 	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
-	pthread_mutex_unlock(&philo->table->eat_mutex);
 }
 
 void ft_sleep(t_philo *philo)
 {
-    size_t i = ft_gettime() - philo->table->start_time;
-	printf("%ld %d is sleeping\n",i , philo->id);
+	ft_printstate("is sleeping\n", philo->id, philo->table->start_time);
 	usleep(philo->table->time_to_sleep * 1000);
 }
 
 void ft_think(t_philo *philo)
 {
-    size_t i = ft_gettime() - philo->table->start_time;
-	printf("%ld %d is thinking\n",i , philo->id);
+	ft_printstate("is thinking\n", philo->id, philo->table->start_time);
 	usleep(700);
 }
